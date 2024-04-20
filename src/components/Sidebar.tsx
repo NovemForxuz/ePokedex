4f9e87b21ca19bf29e409a0a0029f5c1cd5bb990
HerizYusoff <mherizpy@live.com>
2024-04-20 11:32:24 +0800

    Add Sidebar UI menu items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,9 @@
+import CatchingPokemonTwoToneIcon from '@mui/icons-material/CatchingPokemonTwoTone';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import locationIcon from '../assets/icons/mdi_location.png';
+import swordIcon from '../assets/icons/mdi_sword.png';
+
 const Sidebar = () => {
     return (
         <div className="drawer-side z-20">
@@ -6,9 +12,36 @@ const Sidebar = () => {
                 {/* Sidebar content */}
                 <li><h1 className='mx-auto px-auto'><span className='text-red-600'>e</span><span className='text-neutral-600'>Pokedex</span></h1></li>
                 <li><div className="divider m-0"></div> </li>
-                <li><a href="">Pokedex</a></li>
-                <li><a href="">Move Dex</a></li>
-                <li><a href="">Ability Dex</a></li>
+                <li>
+                    <a href="">
+                        <CatchingPokemonTwoToneIcon />
+                        Pokedex
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src={swordIcon} alt="" />
+                        Move Dex
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <AutoAwesomeIcon />
+                        Ability Dex
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src={locationIcon} alt="" />
+                        Ability Dex
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <LocalOfferIcon />
+                        Type Dex
+                    </a>
+                </li>
             </ul>
         </div>
     )
