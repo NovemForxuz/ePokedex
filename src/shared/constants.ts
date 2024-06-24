dc03349136fc12e3350771bd2e292fa247bbd800
HerizYusoff <mherizpy@live.com>
2024-06-24 23:26:10 +0800

    Added constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,3 @@
+export const speciesDescription = "For some time after its birth, it uses the nutrients that are packed into the seed on its back in order to grow.";
+export const speciesWeight = "15.21 lbs (6.9 kg)";
+export const speciesHeight = "2'4\" (0.71 m)";
